ccfb43ac038ec48a3f24aaa66d97e1f379942f53
vovapasko <vovapasko1699@gmail.com>
2021-05-18 16:00:55 +0200

    Completed chapter 2

--- code-ch02/ecc.py
+++ code-ch02/ecc.py
@@ -153,7 +153,7 @@ class Point:
     def __add__(self, other):  # <2>
         if self.a != other.a or self.b != other.b:
             raise TypeError('Points {}, {} are not on the same curve'.format
-            (self, other))
+                            (self, other))
 
         if self.x is None:  # <3>
             return other
@@ -163,19 +163,36 @@ class Point:
 
         # Case 1: self.x == other.x, self.y != other.y
         # Result is point at infinity
-
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
         # Case 2: self.x ≠ other.x
         # Formula (x3,y3)==(x1,y1)+(x2,y2)
         # s=(y2-y1)/(x2-x1)
         # x3=s**2-x1-x2
         # y3=s*(x1-x3)-y1
-
+        if self.x != other.x:
+            x1 = self.x
+            y1 = self.y
+            x2 = other.x
+            y2 = other.y
+            s = (y2 - y1) / (x2 - x1)
+            x3 = s**2 - x1 - x2
+            y3 = s * (x1 - x3) - y1
+            return self.__class__(x3, y3, self.a, self.b)
         # Case 3: self == other
         # Formula (x3,y3)=(x1,y1)+(x1,y1)
         # s=(3*x1**2+a)/(2*y1)
         # x3=s**2-2*x1
         # y3=s*(x1-x3)-y1
-
+        if self == other:
+            x1 = self.x
+            y1 = self.y
+            x3 = other.x
+            y3 = other.y
+            s = (3 * x1**2 + self.a) / (2 * y1)
+            x3 = s**2 - 2 * x1
+            y3 = s * (x1 - x3) - y1
+            return self.__class__(x3, y3, self.a, self.b)
         raise NotImplementedError
 
 
